7abdddcf3e70b682d38adf3e745a1fb5e5a7b72e
Allan Ayford <60179207+allanayford-dev@users.noreply.github.com>
2025-01-11 04:26:23 +0200

    Update API

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const { PORT, NODE_ENV, HOST } = require('./config')
 // Middleware to parse JSON
 app.use(express.json())
 
+app.get('/', (req, res) => {
+	res.send('Welcome to my API.')
+})
+
 // Start the server
 app.listen(PORT, () => {
 	console.log(`Server is running on http://${HOST}:${PORT} in ${NODE_ENV}`)
